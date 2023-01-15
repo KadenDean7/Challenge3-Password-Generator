c69e67db2f926cf9d0d34a25628f87845e3786bb
KadenDean7 <kadenlukasdean@hotmail.com>
2023-01-14 19:05:50 -0600

    added prompts for user input for password specifics

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,33 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+//var for each of the prompts
+var passwordLen = 8;
+var includeUpper = true;
+var includeLower = true;
+var includeNum = true;
+var includeSpecChar = true;
+
 
 // Write password to the #password input
 function writePassword() {
+
+  passwordLen = prompt("Choose password length between 8 and 128.");
+  while(passwordLen < 8 || passwordLen > 128){
+    passwordLen = prompt("ERROR!! Choose password length between 8 and 128.")
+  }
+  includeUpper = prompt("Include uppercase letters? (yes/no)");
+  includeLower = prompt("Include lowercase letters? (yes/no)");
+  includeNum = prompt("Include numbers? (yes/no)");
+  includeSpecChar = prompt("Inlude special characters? (yes/no)");
+
+
+
+  console.log(passwordLen);
+  console.log(includeUpper);
+  console.log(includeLower);
+  console.log(includeNum);
+  console.log(includeSpecChar);
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
